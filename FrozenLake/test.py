311e1fa47553564a56340bfe8b3de663f9ae1310
Maker-Rat <ritwiksha2027@gmail.com>
2023-11-06 20:30:16 +0530

    added test code for FrozenLake

---
+++ FrozenLake/test.py
@@ -0,0 +1,18 @@
+from frozen_lake_custom import FrozenLakeCustomEnv
+import random
+
+Env = FrozenLakeCustomEnv(render_mode="human")
+
+actions = [0, 1, 2, 3]
+
+observation, info = Env.reset()
+
+for _ in range(1000):
+    action = random.choice(actions)
+    observation, reward, terminated, truncated, info = Env.step(action)
+    print(reward)
+
+    if terminated or truncated:
+        observation, info = Env.reset()
+
+Env.close()
